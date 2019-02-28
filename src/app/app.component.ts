d599726f012fb4ee6773450f192acbd4b3c410c0
TONY ADAMS <truvisory@TONYs-MacBook-Pro.local>
2019-02-28 15:02:56 -0700

    Edited app level components.  Created home and product components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MyAngularClient';
+  public title = "Tony's Angular App";
+  public isAuthenticated: boolean;
+
+  constructor() {
+    this.isAuthenticated = false;
+  }
+
+  login() {
+  }
+
+  logout() {
+  }
 }
